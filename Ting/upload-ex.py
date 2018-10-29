44102dd8f75f570c19d137dae2144022259802d3
Xiaobo Hong <xhong@slb.com>
2018-10-29 13:11:55 -0500

    update

--- Ting/upload-ex.py
+++ Ting/upload-ex.py
@@ -66,5 +66,5 @@ application = tornado.web.Application([
 
 print ("Server started.")
 if __name__ == "__main__":
-    application.listen(8888)
+    application.listen(8080)
     tornado.ioloop.IOLoop.instance().start()
